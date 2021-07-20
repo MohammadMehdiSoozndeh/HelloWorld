e60336e79ad955094474c13c0ec7156d317a23ff
Mohammad Mahdi Soozandeh <mo.m10003@gmail.com>
2021-07-20 20:10:55 +0430

    Initial commit

---
+++ src/main/java/com/example/helloworld/Controller.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class Controller {
+
+    @GetMapping("/hi")
+    public String some() {
+        return "Hello World";
+    }
+
+}
